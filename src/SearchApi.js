e958ced4fb3a077b294b2f093f4750d416029927
milan.nemeth@sulid.hu <milan.nemeth@sulid.hu>
2020-09-09 10:58:28 +0200

    Init Project

---
+++ src/SearchApi.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+
+function SearchApi() {
+const {Builder, By, Key, until} = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const webdriver = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const firefox = require('selenium-webdriver/firefox');
+
+(async function example() {
+  let driver = await new Builder().forBrowser('firefox').build();
+  try {
+    await driver.get('http://www.google.com/ncr');
+    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+    await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+  } finally {
+    await driver.quit();
+  }
+})();
+
+    return (
+        <div className="search__app">
+            
+        </div>
+    )
+}
+
+export default SearchApi
